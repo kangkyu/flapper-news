9ee6c2e3d9c6a1e52b68bb668bd9372230418bfe
kangkyu <kangkyu1111@gmail.com>
2015-06-07 22:03:52 -0700

    add app.js setup (hello world state)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  # Rails is serving our AngularJS application instead of the default Rails landing page.
+  # We can do this by creating an action in ApplicationController which will only render the layout,
+  # and then route requests to the root of our Rails app to that action.
+  def index
+    render 'layouts/application'  
+  end
 end
