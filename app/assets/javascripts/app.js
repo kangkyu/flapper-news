54000513c8d148092bc3b86af2ffa5d7eff3d5c4
kangkyu <kangkyu1111@gmail.com>
2015-06-11 13:26:51 -0700

    add resolve property to ui-router state home

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -8,8 +8,12 @@ function($stateProvider, $urlRouterProvider){
     .state('home',{
       url: '/home',
       templateUrl: 'home/_home.html',
-      controller: 'MainCtrl'
-
+      controller: 'MainCtrl',
+      resolve: {
+        postPromise: ['posts', function(posts){
+          return posts.getAll();
+        }]
+      }
     })
     .state('posts',{
       url: '/posts/{id}',
