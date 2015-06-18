4bab321e1a5e30d646563621f022d281aea06f6a
kangkyu <kangkyu1111@gmail.com>
2015-06-18 16:59:12 -0700

    generate migration for user references

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150613211117) do
+ActiveRecord::Schema.define(version: 20150618235112) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "body"
@@ -20,9 +20,11 @@ ActiveRecord::Schema.define(version: 20150613211117) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "author"
+    t.integer  "user_id"
   end
 
   add_index "comments", ["post_id"], name: "index_comments_on_post_id"
+  add_index "comments", ["user_id"], name: "index_comments_on_user_id"
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
@@ -30,7 +32,10 @@ ActiveRecord::Schema.define(version: 20150613211117) do
     t.integer  "upvotes"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
+
+  add_index "posts", ["user_id"], name: "index_posts_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
